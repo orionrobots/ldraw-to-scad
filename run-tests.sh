05453d436acd36044998993694c5581f4637e348
Robert Schiele <rschiele@gmail.com>
2022-05-19 09:38:32 +0200

    enable build of pypi package

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,9 @@
 docker run --rm -v ${PWD}:/mnt/src python:3.9 bash -ceu "
   cd /mnt/src &&
   pip install -r test-requirements.txt &&
-  pytest .
+  pip install -e .
+  python -m pytest
+  python -m pylint src tests ldraw2scad
+  python -m pycodestyle src tests ldraw2scad
+  python -m build
 "
